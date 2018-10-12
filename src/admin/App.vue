15c3f9c51ad8dfe4467036f4fc48484a27712429
chuilee <chui.lee8@gmail.com>
2018-10-12 16:36:50 +0800

    添加后台管理

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,23 +1,50 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-container>
+      <el-header>
+        <el-menu
+          :default-active="activeIndex"
+          class="el-menu-demo"
+          mode="horizontal"
+          :router="true"
+          @select="handleSelect"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <el-menu-item index="userList">用户管理</el-menu-item>
+          <el-menu-item index="material">素材管理</el-menu-item>
+          <el-menu-item index="activityList">活动排行榜</el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-main>
+        <router-view/>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      activeIndex: 'userList'
+    };
+  },
+  created() {
+    this.activeIndex = this.$route.name;
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key)
+    }
+  }
 };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" src="./assets/scss/public.scss"></style>
+<style lang="scss" scoped>
+  .el-header {
+    background: rgb(84, 92, 100);
+  }
 </style>
+
--- src/admin/router/index.js
+++ src/admin/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/admin/components/HelloWorld';
+import UserList from '@/admin/components/UserList';
+import Material from '@/admin/components/Material';
+import ActivityList from '@/admin/components/ActivityList';
 
 Vue.use(Router);
 
@@ -8,8 +10,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'userList',
+      component: UserList,
+    },
+    {
+      path: '/userList',
+      name: 'userList',
+      component: UserList,
+    },
+    {
+      path: '/material',
+      name: 'material',
+      component: Material,
+    },
+    {
+      path: '/activityList',
+      name: 'activityList',
+      component: ActivityList,
     },
   ],
 });
